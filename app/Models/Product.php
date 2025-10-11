7da3638872646859a2e7bda9427f8dad8733349d
Erick Cordeiro <erickcordeiroa@gmail.com>
2025-10-11 19:26:03 -0300

    feat: Created Model Category and Product

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'category_id',
+        'label',
+        'description',
+        'price',
+        'sales_count',
+        'views_count'
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
